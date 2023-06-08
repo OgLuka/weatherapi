5605d9b817c7120360ffffaab64f26ddb0712654
OgLuka <73040859+OgLuka@users.noreply.github.com>
2023-06-08 13:48:22 +0400

    Update WeatherInfo.tsx

--- src/components/WeatherInfo.tsx
+++ src/components/WeatherInfo.tsx
@@ -76,7 +76,7 @@ export default function WeatherInfo({ weather }: WeatherInfoProps) {
         <AnimatePresence>
           {
             weatherData?.map((data, index) => {
-              if (data.value) {
+              if (!data.value.indexOf(“undefined”) || !data.value.indexOf(“null”)) {
               return (<motion.div 
                 initial={{ opacity: 0, y: 0 }}
                 animate={{ opacity: 1, y: 0 }}
